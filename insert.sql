a10c177f19f63021362b5755c8306e21dee1fec3
superneyluj <julien.sicot@etu.univ-cotedazur.fr>
2022-09-16 09:11:07 +0200

    correction du mot values

--- insert.sql
+++ insert.sql
@@ -6,22 +6,22 @@ insert into actor
 	values (1,'di caprio','leonardo','47','M','Américaine')
 
 insert into actor
-	value(2,'damon','matt','51','M','Américaine')
+	values(2,'damon','matt','51','M','Américaine')
 
 insert into actor
-	value(3,'scarlett','johansson','37','F','Américaine')
+	values(3,'scarlett','johansson','37','F','Américaine')
 
 insert into actor
-	value(4,'charlize','theron','47','F','Américaine')
+	values(4,'charlize','theron','47','F','Américaine')
 
 insert into actor
-	value(5,'alain','delon','86','M','Française')
+	values(5,'alain','delon','86','M','Française')
 
 insert into actor
-	value(6,'catherine','deneuve','78','Française')
+	values(6,'catherine','deneuve','78','Française')
 
 insert into actor
-	value(7,'will','smith','53','Américaine')
+	values(7,'will','smith','53','Américaine')
 
 insert into actor
-	value(8,'emma','stone','33','Amricaine');
+	values(8,'emma','stone','33','Amricaine');
