dc7464654fdf9be3db83362fb92cbc1918763d9f
superneyluj <julien.sicot@etu.univ-cotedazur.fr>
2022-09-16 09:08:53 +0200

    insert into actorr

--- insert.sql
+++ insert.sql
@@ -1,16 +1,27 @@
 -- script création d'items dans la base de données
 
-insert into actor
-	values (1,'di caprio','leonardo',47,'M','Américain')
+use versus; 
 
 insert into actor
-	value(2,'damon','matt',51,'M','Américain')
+	values (1,'di caprio','leonardo','47','M','Américaine')
 
 insert into actor
-	value(3,'scarlett','johansson',37,'F','Américaine')
+	value(2,'damon','matt','51','M','Américaine')
 
 insert into actor
-	value(4,'charlize','theron',47,'F','Américaine')
+	value(3,'scarlett','johansson','37','F','Américaine')
 
 insert into actor
-	value(5,'')
+	value(4,'charlize','theron','47','F','Américaine')
+
+insert into actor
+	value(5,'alain','delon','86','M','Française')
+
+insert into actor
+	value(6,'catherine','deneuve','78','Française')
+
+insert into actor
+	value(7,'will','smith','53','Américaine')
+
+insert into actor
+	value(8,'emma','stone','33','Amricaine');
