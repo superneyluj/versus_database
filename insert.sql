dfbffa6508d67f16ff8029ea104e52bc1664c983
superneyluj <julien.sicot@etu.univ-cotedazur.fr>
2022-09-16 09:39:54 +0200

    correction de bugs scrip acteurs

--- insert.sql
+++ insert.sql
@@ -3,25 +3,25 @@
 use versus; 
 
 insert into actor
-	values (1,'di caprio','leonardo','47','M','Américaine')
+	values (1,'di caprio','leonardo','47','M','Américaine');
 
 insert into actor
-	values(2,'damon','matt','51','M','Américaine')
+	values(2,'damon','matt','51','M','Américaine');
 
 insert into actor
-	values(3,'scarlett','johansson','37','F','Américaine')
+	values(3,'scarlett','johansson','37','F','Américaine');
 
 insert into actor
-	values(4,'charlize','theron','47','F','Américaine')
+	values(4,'charlize','theron','47','F','Américaine');
 
 insert into actor
-	values(5,'alain','delon','86','M','Française')
+	values(5,'alain','delon','86','M','Française');
 
 insert into actor
-	values(6,'catherine','deneuve','78','Française')
+	values(6,'catherine','deneuve','78''F','Française');
 
 insert into actor
-	values(7,'will','smith','53','Américaine')
+	values(7,'will','smith','53','M','Américaine');
 
 insert into actor
-	values(8,'emma','stone','33','Amricaine');
+	values(8,'emma','stone','33','F','Amricaine');
