17996a28428f496ff03309bd25624d462f779b2d
superneyluj <julien.sicot@etu.unice.fr>
2022-09-21 21:13:35 +0200

    code excel fonctionnel, il faut corriger les erreurs de variables durée

---
+++ insert_into.sql
@@ -0,0 +1,224 @@
+INSERT INTO utilisateur
+VALUES( 1,sicot,julien,M,sicot.julien@polytech.com,16/07/1997,sicot.julien,ZCD90dgi,France);
+INSERT INTO utilisateur
+VALUES( 2,jorand,antoine,M,jorand.antoine@polytech.com,31/01/2004,jorand.antoine,ZEO83xfl,France);
+INSERT INTO utilisateur
+VALUES( 3,martino,ugo,M,martino.ugo@polytech.com,14/12/2001,martino.ugo,VSJ45kji,France);
+INSERT INTO utilisateur
+VALUES( 4,prevoteau,sullivan,M,prevoteau.sullivan@polytech.com,30/07/1998,prevoteau.sullivan,WSB11wdv,France);
+INSERT INTO utilisateur
+VALUES( 5,allouis,ambre,F,allouis.ambre@polytech.com,17/12/2004,allouis.ambre,DSE74ieg,France);
+INSERT INTO utilisateur
+VALUES( 6,bensalem,ilyes,M,bensalem.ilyes@polytech.com,15/09/2004,bensalem.ilyes,UDM20irj,France);
+INSERT INTO utilisateur
+VALUES( 7,bousquet,alexandre,M,bousquet.alexandre@polytech.com,03/06/2002,bousquet.alexandre,SFD24fkw,France);
+INSERT INTO utilisateur
+VALUES( 8,canella,clément,M,canella.clément@polytech.com,20/07/2000,canella.clément,KEG71ocy,France);
+INSERT INTO utilisateur
+VALUES( 9,frantzen,maxime,M,frantzen.maxime@polytech.com,02/11/2003,frantzen.maxime,NTG81uvt,France);
+INSERT INTO utilisateur
+VALUES( 10,grondin,clément,M,grondin.clément@polytech.com,16/02/1998,grondin.clément,ZZA61anl,France);
+INSERT INTO utilisateur
+VALUES( 11,guillaume,solène,F,guillaume.solène@polytech.com,29/12/1999,guillaume.solène,JFC49vpb,France);
+INSERT INTO utilisateur
+VALUES( 12,lecuivre,antoine,M,lecuivre.antoine@polytech.com,24/10/2002,lecuivre.antoine,FEO58soi,France);
+INSERT INTO utilisateur
+VALUES( 13,moreau,jules,M,moreau.jules@polytech.com,15/10/1998,moreau.jules,KVT06muk,France);
+INSERT INTO utilisateur
+VALUES( 14,rguibi,taha,M,rguibi.taha@polytech.com,20/12/1998,rguibi.taha,NDO07wej,France);
+INSERT INTO utilisateur
+VALUES( 15,scanzalupi,alexandre,M,scanzalupi.alexandre@polytech.com,07/09/1996,scanzalupi.alexandre,UEN71dgc,France);
+INSERT INTO utilisateur
+VALUES( 16,benzaouia,oussama,M,benzaouia.oussama@polytech.com,02/01/2003,benzaouia.oussama,EFF40yfk,France);
+INSERT INTO utilisateur
+VALUES( 17,robert,sylvain,M,robert.sylvain@polytech.com,02/07/1996,robert.sylvain,VQH77dwz,France);
+
+INSERT INTO movie
+VALUES( 2,inception,0,102777777777778,christopher ,nolan,l'histoire d'un mec qui rêve,sci-fi);
+INSERT INTO movie
+VALUES( 3,tenet,0,104166666666667,christopher ,nolan,voyage dans le temps,sci-fi);
+INSERT INTO movie
+VALUES( 4,american psycho,7,01388888888889E-02,marry,haron,psychopathe en vadrouille,drame);
+INSERT INTO movie
+VALUES( 5,shining,8,26388888888889E-02,stanley,kubrick,hache + porte,horreur);
+INSERT INTO movie
+VALUES( 6,orange mécanique,9,44444444444444E-02,stanley,kubrick,violence,drame);
+INSERT INTO movie
+VALUES( 7,les affranchis,0,101388888888889,martin,scorsese,je suis marrant ?,gangsters);
+INSERT INTO movie
+VALUES( 8,casino,0,123611111111111,martin,scorsese,stylo, gorge,gangsters);
+INSERT INTO movie
+VALUES( 9,seul sur mars,9,79166666666667E-02,ridley,scott,solo,sci-fi);
+INSERT INTO movie
+VALUES( 10,transcendance,8,26388888888889E-02,wally,pfister,cerveau -> ordinateur,sci-fi);
+
+INSERT INTO actor
+VALUES( 2,leonardo,di caprio,47,M,américain);
+INSERT INTO actor
+VALUES( 3,bob,odenkirk,59,M,américain);
+INSERT INTO actor
+VALUES( 4,john david,washington,38,M,américain);
+INSERT INTO actor
+VALUES( 5,jack,nicholson,85,M,américain);
+INSERT INTO actor
+VALUES( 6,christian,bale,48,M,américain);
+INSERT INTO actor
+VALUES( 7,warren,clarke,88,M,américain);
+INSERT INTO actor
+VALUES( 8,robert,de niro,79,M,américain);
+INSERT INTO actor
+VALUES( 9,joe,pesci,79,M,américain);
+INSERT INTO actor
+VALUES( 10,matt ,damon,51,M,américain);
+INSERT INTO actor
+VALUES( 11,johny ,depp,59,M,américain);
+INSERT INTO actor
+VALUES( 12,bryan,cranston,66,M,américain);
+INSERT INTO actor
+VALUES( 13,damian,lewis,51,M,américain);
+INSERT INTO actor
+VALUES( 14,jared,harris,61,M,américain);
+INSERT INTO actor
+VALUES( 15,steve,carell,60,M,américain);
+INSERT INTO actor
+VALUES( 16,idris,elba,50,M,américain);
+INSERT INTO actor
+VALUES( 17,woody,harrelson,61,M,américain);
+
+INSERT INTO tv_show
+VALUES( 2,band of brothers,tom,hanks,des potes qui font la guerre,guerre);
+INSERT INTO tv_show
+VALUES( 3,chernobyl,craig,mazin,une centrale nucléaire qui explose,drame historique);
+INSERT INTO tv_show
+VALUES( 4,the wire,david,simon,la police et les voleurs,policière);
+INSERT INTO tv_show
+VALUES( 5,les sopranos,david,chase,les mafieux,drame);
+INSERT INTO tv_show
+VALUES( 6,game of thrones,david,benioff,des dragons et de la guerre,fantaisie);
+INSERT INTO tv_show
+VALUES( 7,rick et morty,dan ,harmon,un grand père et son petit fils,sci-fi);
+INSERT INTO tv_show
+VALUES( 8,the office,ricky ,gervais,un manager débile,sitcom);
+INSERT INTO tv_show
+VALUES( 9,true detective,nic,pizzolatto,les gentils flics,policière);
+INSERT INTO tv_show
+VALUES( 10,better call saul,vince,gilligan,un avocat véreux,drame);
+
+INSERT INTO platform
+VALUES( 1,netflix, 17,99 );
+INSERT INTO platform
+VALUES( 2,amazon prime, 6,99 );
+INSERT INTO platform
+VALUES( 3,disney+, 8,99 );
+INSERT INTO platform
+VALUES( 4,mycanal, 24,99 );
+INSERT INTO platform
+VALUES( 5,ocs, 10,99 );
+INSERT INTO platform
+VALUES( 6,appletv+, 4,99 );
+INSERT INTO platform
+VALUES( 7,salto, 6,99 );
+
+INSERT INTO subscription
+VALUES( 1,2,17,27/08/2021 06:12:02,mastercard);
+INSERT INTO subscription
+VALUES( 2,5,1,15/05/2022 23:37:57,paypal);
+INSERT INTO subscription
+VALUES( 3,5,3,24/01/2021 08:10:16,visa);
+INSERT INTO subscription
+VALUES( 4,5,1,03/07/2020 21:53:13,mastercard);
+INSERT INTO subscription
+VALUES( 5,4,1,25/12/2021 15:56:49,paypal);
+INSERT INTO subscription
+VALUES( 6,3,9,25/02/2020 11:22:42,bitcoin);
+INSERT INTO subscription
+VALUES( 7,6,10,16/02/2021 23:13:50,paypal);
+INSERT INTO subscription
+VALUES( 8,7,1,19/06/2020 08:33:03,mastercard);
+INSERT INTO subscription
+VALUES( 9,7,7,02/10/2021 14:15:33,bitcoin);
+INSERT INTO subscription
+VALUES( 10,6,8,06/06/2022 11:41:32,paypal);
+
+INSERT INTO tv_show_viewing
+VALUES( 2,6,1,3,68211253670186E-03,11/04/2020 00:20:51,téléphone,78,2, 7 );
+INSERT INTO tv_show_viewing
+VALUES( 3,9,15,9,9346024717438E-04,06/09/2021 14:58:04,tv,78,4, 2 );
+INSERT INTO tv_show_viewing
+VALUES( 4,6,15,2,47791822075811E-02,17/05/2021 19:45:41,tablette,68,3, 10 );
+INSERT INTO tv_show_viewing
+VALUES( 5,5,8,1,91507958490052E-02,12/03/2022 05:11:30,tv,41,5, 5 );
+INSERT INTO tv_show_viewing
+VALUES( 6,6,4,1,41036564231845E-03,04/09/2020 03:11:33,tv,43,1, 4 );
+INSERT INTO tv_show_viewing
+VALUES( 7,5,2,2,51039698668964E-02,09/10/2020 06:53:47,tv,100,5, 12 );
+INSERT INTO tv_show_viewing
+VALUES( 8,6,13,3,45634847835432E-03,26/05/2022 19:33:03,tablette,51,4, 2 );
+INSERT INTO tv_show_viewing
+VALUES( 9,5,16,2,62269806204096E-02,29/01/2022 09:00:19,tablette,90,3, 9 );
+INSERT INTO tv_show_viewing
+VALUES( 10,4,16,2,38569873965186E-02,28/10/2021 05:31:49,tv,81,1, 1 );
+INSERT INTO tv_show_viewing
+VALUES( 11,7,3,9,44743294716207E-03,30/05/2021 00:15:37,téléphone,64,5, 2 );
+INSERT INTO tv_show_viewing
+VALUES( 12,7,7,2,20386621017356E-02,17/02/2022 15:33:00,tv,53,3, 3 );
+
+INSERT INTO movie_viewing
+VALUES( 2,1,8,8,66921850894268E-02,29/04/2020 15:49:51,téléphone, 77 );
+INSERT INTO movie_viewing
+VALUES( 3,9,4,0,171127820972705,12/08/2020 10:38:33,tv, 23 );
+INSERT INTO movie_viewing
+VALUES( 4,5,8,0,229868168097065,20/07/2021 17:08:00,tablette, 31 );
+INSERT INTO movie_viewing
+VALUES( 5,6,8,0,159287258280183,14/12/2020 11:12:53,tv, 38 );
+INSERT INTO movie_viewing
+VALUES( 6,9,10,3,13817850210253E-02,12/08/2022 19:13:08,tv, 11 );
+INSERT INTO movie_viewing
+VALUES( 7,1,4,0,149983478825333,12/07/2020 11:03:38,tv, 15 );
+INSERT INTO movie_viewing
+VALUES( 8,5,10,0,139340980394834,13/11/2021 00:23:51,tablette, 84 );
+INSERT INTO movie_viewing
+VALUES( 9,6,16,0,224055760227871,25/01/2020 20:29:16,tablette, 72 );
+INSERT INTO movie_viewing
+VALUES( 10,8,6,0,102923644899153,01/12/2020 16:52:55,tv, 40 );
+INSERT INTO movie_viewing
+VALUES( 11,9,8,0,193891307196371,30/08/2020 18:50:44,téléphone, 71 );
+INSERT INTO movie_viewing
+VALUES( 12,10,7,0,291116134464446,24/11/2021 12:53:26,tv, 93 );
+
+INSERT INTO movie_disponibility
+VALUES( 2,2, 1 );
+INSERT INTO movie_disponibility
+VALUES( 3,3, 3 );
+INSERT INTO movie_disponibility
+VALUES( 4,4, 4 );
+INSERT INTO movie_disponibility
+VALUES( 5,5, 5 );
+INSERT INTO movie_disponibility
+VALUES( 6,6, 6 );
+INSERT INTO movie_disponibility
+VALUES( 7,7, 1 );
+INSERT INTO movie_disponibility
+VALUES( 8,8, 6 );
+INSERT INTO movie_disponibility
+VALUES( 9,9, 5 );
+INSERT INTO movie_disponibility
+VALUES( 10,10, 6 );
+INSERT INTO tv_show_disponibility
+VALUES( 2,2, 6 );
+INSERT INTO tv_show_disponibility
+VALUES( 3,3, 4 );
+INSERT INTO tv_show_disponibility
+VALUES( 4,4, 4 );
+INSERT INTO tv_show_disponibility
+VALUES( 5,5, 1 );
+INSERT INTO tv_show_disponibility
+VALUES( 6,6, 4 );
+INSERT INTO tv_show_disponibility
+VALUES( 7,7, 3 );
+INSERT INTO tv_show_disponibility
+VALUES( 8,8, 4 );
+INSERT INTO tv_show_disponibility
+VALUES( 9,9, 3 );
+INSERT INTO tv_show_disponibility
+VALUES( 10,10, 2 );
